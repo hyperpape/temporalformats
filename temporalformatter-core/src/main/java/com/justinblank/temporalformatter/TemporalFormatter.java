e5f12bea11ab4f11773e724a07b5483ab216d3bf
Justin Blank <justin.blank@gmail.com>
2022-05-16 10:23:39 -0400

    Document thread-safety

--- temporalformatter-core/src/main/java/com/justinblank/temporalformatter/TemporalFormatter.java
+++ temporalformatter-core/src/main/java/com/justinblank/temporalformatter/TemporalFormatter.java
@@ -2,6 +2,9 @@ package com.justinblank.temporalformatter;
 
 import java.time.temporal.TemporalAccessor;
 
+/**
+ * A formatter for TemporalAccessor instances. Implementations of this class must be thread safe.
+ */
 public interface TemporalFormatter {
 
     String format(TemporalAccessor temporal);
